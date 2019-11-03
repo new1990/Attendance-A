a1602cd3d69da4d266023d00099be2a992387413
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-03 06:06:35 +0000

    ログインフォームページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  # ログイン
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#ncreate'
+  get '/logout', to: 'sessions#destroy'
+  
   resources :users
 end
