3a4d5cca4c3cd778023991f7e72eee7b2a7d4360
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-03 06:48:17 +0000

    ログインメソッド追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   
   # ログイン
   get '/login', to: 'sessions#new'
-  get '/login', to: 'sessions#ncreate'
-  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
