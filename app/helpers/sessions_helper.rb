1a19e0d727e883edb6a7aaafd6af502d043c85c7
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-03 07:30:42 +0000

    ログイン、ログアウト基本機能追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,24 @@
 module SessionsHelper
   
+  # ログイン用
   def log_in(user)
     session[:user_id] = user.id
   end
   
+  # ログアウト用 セッションと@current_userを破棄
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  # 現在ログイン中のユーザーの値を返す
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
   
+  # 現在ログイン中のユーザーがいればtrue、いなければfalse
   def logged_in?
     !current_user.nil?
   end
