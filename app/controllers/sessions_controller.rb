49cfb34fbe2fe75d2a3a881b8f4820d906bf067f
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-07 13:51:01 +0000

    ページネーション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       flash[:success] = "ログインしました。"
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = "認証に失敗しました。"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: %i(show edit update)
   before_action :correct_user, only: %i(edit update)
   
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def new
     @user = User.new
   end
@@ -45,6 +49,7 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
