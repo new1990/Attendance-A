4ac9ccb18a31601482e3e53234f3fb38ecfb3a70
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-07 14:10:02 +0000

    ユーザー削除機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i(show edit update)
-  before_action :logged_in_user, only: %i(show edit update)
+  before_action :set_user, only: %i(show edit update destroy)
+  before_action :logged_in_user, only: %i(show edit update destroy)
   before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
   
   def index
     @users = User.paginate(page: params[:page])
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
   private
   
     def user_params
@@ -57,5 +64,9 @@ class UsersController < ApplicationController
     
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
