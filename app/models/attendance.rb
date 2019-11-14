c1069610dcffe99fa84d43be4883fdd3c9e7a28c
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-14 16:56:37 +0000

    勤怠編集バリデーション追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,8 +5,13 @@ class Attendance < ApplicationRecord
   validates :note, length: { maximum: 50 }
   
   validate :finished_at_is_invalid_without_a_started_at
+  validate :started_at_than_finished_at_fast_if_invalid
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です。") if started_at.blank? && finished_at.present?
+  end
+  
+  def started_at_than_finished_at_fast_if_invalid
+    errors.add(:started_at, "より早い退社時間は無効です。") if started_at > finished_at
   end
 end
