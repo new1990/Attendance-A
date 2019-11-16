3cd45fcda2082dd816658d4a9afebb9ea509578e
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-16 01:20:49 +0000

    勤怠更新バリデーション追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,13 +29,18 @@ class AttendancesController < ApplicationController
   
   def update_one_month
     ActiveRecord::Base.transaction do
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      if attendances_invalid?
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
+        end
+        flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+        flash[:danger] = "不正な時間入力がありました、再入力してください。"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
       end
     end
-    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
