e283965a22a7bb1d95927a0d7ece1ff3aa498e51
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-17 02:14:50 +0000

    ログインページ修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   
   def new
+    if logged_in?
+      flash[:success] = "すでにログインしています。"
+      redirect_to current_user
+    end
   end
   
   def create
