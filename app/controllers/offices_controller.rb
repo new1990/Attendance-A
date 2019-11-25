1b849cf1a67087e5303d4d955f8a83d778d305eb
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-11-25 15:10:52 +0000

    拠点一覧ページ新規登録フォーム受け渡し成功

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,15 +1,26 @@
 class OfficesController < ApplicationController
-   before_action :logged_in_user, only: %i(index new create update destroy)
-   before_action :admin_user, only: %i(index new create update destroy)
+   before_action :logged_in_user, only: %i(index new create edit update destroy)
+   before_action :admin_user, only: %i(index new create edit update destroy)
   
   def index
-  end
-  
-  def new
     @office = Office.new
   end
   
+  def new
+    # @office = Office.new
+  end
+  
   def create
+    @office = Office.new(office_params)
+    if @office.save
+      flash[:success] = "拠点登録が完了しました。"
+      redirect_to offices_url
+    else
+      render :index
+    end
+  end
+  
+  def edit
   end
   
   def update
@@ -17,4 +28,10 @@ class OfficesController < ApplicationController
   
   def destroy
   end
+  
+  private
+  
+    def office_params
+      params.require(:office).permit(:office_number, :office_name, :attendance_type)
+    end
 end
