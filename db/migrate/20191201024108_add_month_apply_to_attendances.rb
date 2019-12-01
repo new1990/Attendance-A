7f5bf6b855f965c148f6e669b0d526f619466526
Tadakiyo-Hirano <d85nus03@gmail.com>
2019-12-01 02:56:33 +0000

    勤怠モデルに月の申請、承認カラム追加

---
+++ db/migrate/20191201024108_add_month_apply_to_attendances.rb
@@ -0,0 +1,7 @@
+class AddMonthApplyToAttendances < ActiveRecord::Migration[5.1]
+  def change
+    add_column :attendances, :month_apply, :datetime
+    add_column :attendances, :month_approval, :integer, default: 1
+    add_column :attendances, :month_check, :string, default: "0"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191201021720) do
+ActiveRecord::Schema.define(version: 20191201024108) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20191201021720) do
     t.datetime "updated_at", null: false
     t.integer "superior_id"
     t.string "status"
+    t.datetime "month_apply"
+    t.integer "month_approval", default: 1
+    t.string "month_check", default: "0"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
